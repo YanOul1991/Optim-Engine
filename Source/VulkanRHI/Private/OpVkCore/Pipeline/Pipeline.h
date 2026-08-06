75a347038a9683676b120034dcd3235f728d0088
Yanis <e2073117@cmaisonneuve.qc.ca>
2026-08-05 21:31:05 -0400

    Implemented basic Vulkan pipeline creation function following the vulkan tutorial.

--- Source/VulkanRHI/Private/OpVkCore/Pipeline/Pipeline.h
+++ Source/VulkanRHI/Private/OpVkCore/Pipeline/Pipeline.h
@@ -49,7 +49,7 @@ inline vk::raii::ShaderModule CreateVkShaderModule(const std::vector<char>& byte
   return vk::raii::ShaderModule(device, createInfo);
 }
 
-void CreateShaderStage(const vk::raii::ShaderModule& shaderModule)
+inline void CreateShaderStage(const vk::raii::ShaderModule& shaderModule)
 {
   vk::PipelineShaderStageCreateInfo vertexShaderStageInfo{
     .stage  = vk::ShaderStageFlagBits::eVertex,
@@ -69,4 +69,87 @@ void CreateShaderStage(const vk::raii::ShaderModule& shaderModule)
   };
 }
 
-} // namespace Optim::VK
+/**
+ * Vulkan Tutorial:
+ * https://docs.vulkan.org/tutorial/latest/03_Drawing_a_triangle/02_Graphics_pipeline_basics/02_Fixed_functions.html
+ */
+inline void CreateVulkanGraphicsPipeline(const vk::Extent2D& swapChainExtent, const vk::raii::Device& device)
+{
+  std::vector<vk::DynamicState> dynamicStates = {
+    vk::DynamicState::eViewport,
+    vk::DynamicState::eScissor
+  };
+
+  vk::PipelineDynamicStateCreateInfo dynamicState{
+    .dynamicStateCount = static_cast<uint32_t>(dynamicStates.size()),
+    .pDynamicStates    = dynamicStates.data()
+  };
+
+  // VERTEX INPUT
+  vk::PipelineVertexInputStateCreateInfo vertexInputInfo;
+
+  // INPUT ASSEMBLY
+  vk::PipelineInputAssemblyStateCreateInfo inputAssemblyInfo{
+    .topology = vk::PrimitiveTopology::eTriangleList,
+  };
+
+  // VIEWPORTS & SCISSORS
+  vk::Viewport viewport{ 0.0f, 0.0f, static_cast<float>(swapChainExtent.width), static_cast<float>(swapChainExtent.height), 0.0f, 1.0f };
+  vk::Rect2D   scissor{
+    vk::Offset2D{ 0, 0 },
+    swapChainExtent
+  };
+
+  vk::PipelineViewportStateCreateInfo viewportState{
+    .viewportCount = 1,
+    .scissorCount  = 1
+  };
+
+  // RASTERIZER
+  vk::PipelineRasterizationStateCreateInfo rasterizer{
+    .depthClampEnable        = vk::False,
+    .rasterizerDiscardEnable = vk::False,
+    .polygonMode             = vk::PolygonMode::eFill,
+    .cullMode                = vk::CullModeFlagBits::eBack,
+    .frontFace               = vk::FrontFace::eClockwise,
+    .depthBiasEnable         = vk::False,
+    .lineWidth               = 1.0f
+  };
+
+  // MULTISAMPLING
+  vk::PipelineMultisampleStateCreateInfo multisampling{
+    .rasterizationSamples = vk::SampleCountFlagBits::e1,
+    .sampleShadingEnable  = vk::False
+  };
+
+  // DEPTH & STENCIL TESTING
+  vk::PipelineDepthStencilStateCreateInfo depthStencil;
+
+  // COLOR BLENDING
+  vk::PipelineColorBlendAttachmentState colorBlendAttachement{
+    .blendEnable         = vk::True,
+    .srcColorBlendFactor = vk::BlendFactor::eSrcAlpha,
+    .dstColorBlendFactor = vk::BlendFactor::eOneMinusSrcAlpha,
+    .colorBlendOp        = vk::BlendOp::eAdd,
+    .srcAlphaBlendFactor = vk::BlendFactor::eOne,
+    .dstAlphaBlendFactor = vk::BlendFactor::eZero,
+    .colorWriteMask      = vk::ColorComponentFlagBits::eR |
+                           vk::ColorComponentFlagBits::eG |
+                           vk::ColorComponentFlagBits::eB |
+                           vk::ColorComponentFlagBits::eA
+  };
+
+  vk::PipelineColorBlendStateCreateInfo colorBlending{
+    .logicOpEnable   = vk::False,
+    .logicOp         = vk::LogicOp::eCopy,
+    .attachmentCount = 1,
+    .pAttachments    = &colorBlendAttachement
+  };
+
+  // PIPELINE LAYOUT
+  vk::raii::PipelineLayout     pipelineLayout = nullptr;
+  vk::PipelineLayoutCreateInfo pipelineLayoutInfo{ .setLayoutCount = 0, .pushConstantRangeCount = 0 };
+  pipelineLayout = vk::raii::PipelineLayout(device, pipelineLayoutInfo);
+}
+
+} // namespace Optim::VKPipeline
